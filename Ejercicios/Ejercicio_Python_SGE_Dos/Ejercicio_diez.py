aa0e7ed4518a8e7034bcc0ebd9c91d8bc3593239
YLJOSE <joserojasr47@gmail.com>
2025-02-11 15:18:19 +0100

    revisar ultimo ejercicio

--- Ejercicios/Ejercicio_Python_SGE_Dos/Ejercicio_diez.py
+++ Ejercicios/Ejercicio_Python_SGE_Dos/Ejercicio_diez.py
@@ -3,13 +3,25 @@
 #Un método mover(x,y), que mueva las coordenadas
 #Un método distancia(p,q) que muestre la distancia entre p y q.
 #  Pista: La distancia entre dos puntos se representa por este ecuación
+import math
 
 class punto:
-
+    def __init__(self):
+        self.x = 1
+        self.y = 3
     def __str__(self):
-        pass
+        return f"Coordenadas en X= {self.x} y en Y= {self.y}"
     def mover(self,x,y):
-        print()
-    def distancia(self,p,q):
-        print()
+        self.x = x
+        self.y = y
+        print(f"Coordenadas actualizadas en X= {self.x} y en Y= {self.y}")
+    def distancia(self,p1,p2):
+        print("revisar")
+
+
+mi_punto = punto()
+
+print(mi_punto.__str__())
+mi_punto.mover(3,4)
+
 
